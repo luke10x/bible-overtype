fcfeb240058dab2c9d89646e76105492edf18a82
Luke 10X <spam@luke10x.com>
2021-09-13 03:20:33 -0500

    Autotext added for line numbers and empty spaces

--- retype.c
+++ retype.c
@@ -1,5 +1,5 @@
 /*
- * while inotifywait -e close_write retype.c; do gcc -Wall -o retype retype.c $(ncursesw5-config --cflags --libs) ; done 
+ * gcc -Wall -o retype retype.c -lutf8proc -lunistring $(ncursesw5-config --cflags --libs)
  * indent -kr -ts4 -nut -l80 *.c
  */
 
@@ -26,7 +26,8 @@
 #define GOOD_PAIR    6
 #define ERROR_PAIR    7
 
-#define MAX_LEN 256
+#define MAX_LEN 81
+#define MAX_LINES 99999
 
 int get_padding(int longest_line, int term_cols);
 
@@ -37,6 +38,8 @@ void re_type(const int row, const int col, const char *line,
 static void sig_handler(int sig);
 
 WINDOW *pad;
+
+FILE *logger;
 
 size_t smartlen(const char *line)
 {
@@ -54,11 +57,13 @@ int main(void)
 
     int longest_line = 0;
 
-    char ch_arr[MAX_LEN][MAX_LEN];
+    char ch_arr[MAX_LINES][MAX_LEN];
 
     FILE *fp;
     // fp = fopen("teve-musu.txt", "r");
     fp = stdin;
+
+    logger = fopen("retype.log", "a+");
 
     if (fp == NULL) {
         perror("Failed: ");
@@ -72,6 +77,10 @@ int main(void)
         buffer[strcspn(buffer, "\n")] = 0;
         strcpy(ch_arr[i], buffer);
         i++;
+        if (i > 32767) {
+            perror("Failed file too long");
+            exit(1);
+        }
 
         if (strlen(buffer) > longest_line) {
             longest_line = strlen(buffer);
@@ -182,12 +191,12 @@ uint8_t normalize(const uint32_t c)
     return *output;
 }
 
-int isssame(const uint32_t expected_ch, const int typed_ch)
+int isssame(const uint32_t expected_ch, int typed_ch)
 {
     uint8_t expected_nch = normalize(expected_ch);
     uint8_t typed_nch = normalize(typed_ch);
 
-    fwprintf(stderr, L"%lc:%d(%d) == %lc:%d(%d) \n",
+    fwprintf(logger, L"%lc:%d(%d) == %lc:%d(%d) \n",
              expected_ch, expected_ch, expected_nch,
              typed_ch, typed_ch, typed_nch);
 
@@ -202,6 +211,52 @@ const uint32_t simplify(const char *input, const int index)
     return unicode;
 }
 
+int this_is_lnumber_start(const char *line, int typed)
+{
+    const uint32_t expected_ch = simplify(line, typed);
+    if (expected_ch >= 0x0030 && expected_ch <= 0x0039) {
+        return true;
+    }
+    return false;
+}
+
+int this_is_subsequent_space(const char *line, int typed)
+{
+    if (typed == 0)
+        return false;
+    const uint32_t previous_ch = simplify(line, typed - 1);
+    const uint32_t expected_ch = simplify(line, typed);
+    if (previous_ch == 0x0020 && expected_ch == 0x0020) {
+        return true;
+    }
+    return false;
+}
+
+bool should_autotext(int now_started, const char *line, int typed,
+                     struct charstack *undostack)
+{
+    if (undostack != 0) {
+        return false;
+    }
+    if (now_started) {
+        const uint32_t expected_ch = simplify(line, typed);
+        if (expected_ch == 0x0020 ||
+            expected_ch == 0x003A ||
+            (expected_ch >= 0x0030 && expected_ch <= 0x0039)
+            ) {
+            return true;
+        }
+    } else {
+        if (this_is_lnumber_start(line, typed)) {
+            return true;
+        }
+        if (this_is_subsequent_space(line, typed)) {
+            return true;
+        }
+    }
+    return false;
+}
+
 void re_type(const int row, const int col, const char *line,
              const int offset, const struct winsize w)
 {
@@ -211,9 +266,23 @@ void re_type(const int row, const int col, const char *line,
     prefresh(pad, offset, 0, 0, 0, w.ws_row - 1, w.ws_col - 1);
 
     int typed = 0;
-    do {
-        wint_t typed_ch = getwchar();
 
+    bool autotext_started = false;
+    do {
+        const uint32_t expected_ch = simplify(line, typed);
+        // fwprintf(logger, L"%d. %lsc(%d) TEST: %ls(%d)\n", typed,
+        // expected_ch, (int)expected_ch);
+        fwprintf(logger, L"----- \n");
+        fwprintf(logger, L"%d. TEST %lc(%d) \n", typed, expected_ch,
+                 expected_ch);
+
+        autotext_started =
+            should_autotext(autotext_started, line, typed, undostack);
+        wint_t typed_ch = autotext_started ? expected_ch : getwchar();
+
+        if (typed_ch == '\t') {
+            typed_ch = 0x20;
+        }
         // ANY control char
         if (typed_ch == 27) {
             endwin();
@@ -222,12 +291,12 @@ void re_type(const int row, const int col, const char *line,
         // ENTER key
         if (typed_ch == 13) {
 
-            fwprintf(stderr, L"typed: %d\n", typed);
-            fwprintf(stderr, L"normal length: %zu\n", strlen(line));
+            fwprintf(logger, L"typed: %d\n", typed);
+            fwprintf(logger, L"normal length: %zu\n", strlen(line));
 
             const size_t i = smartlen(line);
 
-            fwprintf(stderr, L"smarte length: %zu\n", i);
+            fwprintf(logger, L"smarte length: %zu\n", i);
             if (typed < i || undostack != NULL) {
                 continue;
             }
@@ -260,13 +329,6 @@ void re_type(const int row, const int col, const char *line,
             prefresh(pad, offset, 0, 0, 0, w.ws_row - 1, w.ws_col - 1);
             continue;
         }
-
-        const uint32_t expected_ch = simplify(line, typed);
-        // fwprintf(stderr, L"%d. %lsc(%d) TEST: %ls(%d)\n", typed,
-        // expected_ch, (int)expected_ch);
-        fwprintf(stderr, L"----- \n");
-        fwprintf(stderr, L"%d. TEST %lc(%d) \n", typed, expected_ch,
-                 expected_ch);
 
         const int same = isssame(expected_ch, typed_ch);
         // const char testch = winch(pad) & A_CHARTEXT;
